ed3f249d44b2be74f6af28959cdb9547becbe7a9
pjpure <pure254326@gmail.com>
2022-02-25 01:29:32 +0700

    update

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,11 +2,13 @@ import express, { Application } from 'express';
 import router from './routes';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT: number = 8000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
